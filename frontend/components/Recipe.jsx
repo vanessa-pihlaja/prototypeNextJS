6b693fd5ccd003d0574bddd74974dd0c7f399391
vanessa-pihlaja <vanessa.pihlaja@outlook.com>
2024-03-27 19:28:33 +0200

    Added back button for recipe page

--- frontend/components/Recipe.jsx
+++ frontend/components/Recipe.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import styles from '../styles/recipe.module.css';
+import styles from '../styles/recipe.module.css'; // Make sure your styles are correctly linked
 import Image from 'next/image';
 import SaveRecipeModal from './SaveButton';
 
@@ -12,8 +12,8 @@ export default function Recipe() {
     const { title } = router.query;
 
     const handleSaveClick = (recipe) => {
-      setCurrentRecipe(recipe);
-      setShowSaveModal(true);
+        setCurrentRecipe(recipe);
+        setShowSaveModal(true);
     };
 
     useEffect(() => {
@@ -36,9 +36,8 @@ export default function Recipe() {
     };
 
     function stripHtml(html) {
-        // This is a simplistic method and might not handle all edge cases.
-        return html.replace(/<[^>]*>?/gm, '')
-      }      
+        return html.replace(/<[^>]*>?/gm, '');
+    }      
 
     const renderContent = (content) => {
         const lines = stripHtml(content).split('\n').filter(line => line.trim() !== '');
@@ -51,28 +50,39 @@ export default function Recipe() {
 
     return (
         <div className={styles.recipeContainer}>
-          <h1 className={styles.recipeTitle}>{recipe.title}</h1>
-          {recipe.images && recipe.images.length > 0 && (
-        <div className={styles.recipeImage}>
-          <Image
-            src={getFirstImageUrl(recipe.images)}
-            alt={recipe.title}
-            width={150}
-            height={300}
-            layout="responsive"
-            blurDataURL={'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=='}
-            placeholder='blur'
-            priority
-          />
-          <div className={styles.buttonContainer}>
-            <button className={styles.buttonAtFirst} onClick={() => handleSaveClick(recipe)}>Tallenna</button>
-          </div>
-          {showSaveModal && <SaveRecipeModal recipe={currentRecipe} setShowSaveModal={setShowSaveModal} />}
+            <div className={styles.buttonContainer}>
+                <button
+                    className={styles.backButton}
+                    onClick={() => router.back()}
+                    aria-label="Back"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="44" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.backIcon}>
+                        <path d="M19 12H5"></path>
+                        <polyline points="12 19 5 12 12 5"></polyline>
+                    </svg>
+                </button>
+            </div>
+            <h1 className={styles.recipeTitle}>{recipe.title}</h1>
+            {recipe.images && recipe.images.length > 0 && (
+                <div className={styles.recipeImage}>
+                    <Image
+                        src={getFirstImageUrl(recipe.images)}
+                        alt={recipe.title}
+                        width={150}
+                        height={300}
+                        layout="responsive"
+                        blurDataURL={'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=='}
+                        placeholder='blur'
+                        priority
+                    />
+                </div>
+            )}
+            <div className={styles.buttonContainer}>
+                <button className={styles.buttonAtFirst} onClick={() => handleSaveClick(recipe)}>Tallenna</button>
+            </div>
+            {showSaveModal && <SaveRecipeModal recipe={currentRecipe} setShowSaveModal={setShowSaveModal} />}
+            <p className={styles.recipeContent}>{renderContent(recipe.content)}</p>
+            <p><a href={recipe.url} target="_blank" rel="noopener noreferrer">Katso alkuperäinen resepti</a></p>
         </div>
-        
-      )}
-          <p className={styles.recipeContent}>{renderContent(recipe.content)}</p>
-          <p><a href={recipe.url} target="_blank" rel="noopener noreferrer">Katso alkuperäinen resepti</a></p>
-        </div>
-      )
+    );
 }
