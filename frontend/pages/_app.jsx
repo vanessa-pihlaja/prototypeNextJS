6f090f47636be57533df77b842dfb4afeac2348d
vanessa-pihlaja <vanessa.pihlaja@outlook.com>
2024-03-29 13:07:32 +0200

    Added Vercel Web Analytics

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,7 +1,8 @@
 import '../styles/global.css';
 import { UserProvider } from '../contexts/UserContext';
+import { Analytics } from "@vercel/analytics/react";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, Analytics }) {
   return (
     <UserProvider>
       <Component {...pageProps} />
