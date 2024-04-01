b33d6cc24d5d2d9ac8223f48a8825147a4c427f6
vanessa-pihlaja <vanessa.pihlaja@outlook.com>
2024-04-01 09:43:17 +0300

    Fixed deployment error

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -6,7 +6,6 @@ import Script from 'next/script';
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
         {/* Google tag (gtag.js) */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DLHKESPVZT"></Script>
         <Script
@@ -21,8 +20,6 @@ export default function MyApp({ Component, pageProps }) {
             `,
           }}
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      </Head>
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
